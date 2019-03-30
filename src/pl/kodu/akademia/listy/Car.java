2d5012840d4a8a0f410d1467746ae4a8d578ce7a
Moozgu <rukagilga@gmail.com>
2019-03-30 12:38:46 +0100

    Messing stuff up even more

--- src/pl/kodu/akademia/listy/Car.java
+++ src/pl/kodu/akademia/listy/Car.java
@@ -1,5 +1,7 @@
 package pl.kodu.akademia.listy;
 
+import java.util.Objects;
+
 public class Car {
     private String brand;
     private int year;
@@ -27,9 +29,24 @@ public class Car {
 
     @Override
     public String toString() {
-        return "Car {" +
-                "brand='" + brand + '\'' +
-                ", year=" + year +
-                '}';
+        StringBuilder builder = new StringBuilder();
+        builder.append(this.brand);
+        builder.append(", ");
+        builder.append(this.year);
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year &&
+                Objects.equals(brand, car.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, year);
     }
 }
--- src/pl/kodu/akademia/sety/Main.java
+++ src/pl/kodu/akademia/sety/Main.java
@@ -1,4 +1,18 @@
 package pl.kodu.akademia.sety;
 
+import pl.kodu.akademia.listy.Car;
+
 public class Main {
+    public static void main(String[] args) {
+        Car a = new Car("BMW", 1999);
+        Car b = new Car("BMW", 1999);
+//        Car b = a;
+
+//        a.setBrand("Audi");
+
+        System.out.println(a);
+        System.out.println(b);
+//        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
 }
